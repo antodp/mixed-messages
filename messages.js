a40b2cf1bdc44d3e2b6e12d23ac8366c453c846c
Antonio <antodp9@gmail.com>
2021-08-10 10:20:06 +0200

    first commit

---
+++ messages.js
@@ -0,0 +1,16 @@
+const planets = ["Mars", "Venus", "Jupiter", "Saturn"];
+const seasons = ["Spring", "Summer", "Winter", "Fall"];
+const times = ["Morning", "Day", "Evening", "Night"];
+
+const factoryUser = name => {
+    return {
+        name,
+        planet: planets[Math.floor(Math.random()*4)],
+        season: seasons[Math.floor(Math.random()*4)],
+        time: times[Math.floor(Math.random()*4)]
+    }
+}
+
+const Antonio = factoryUser("Antonio");
+console.log(Antonio);
+
